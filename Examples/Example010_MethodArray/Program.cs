771a8138a56c6c853dc3638d1c3659ed2d4f0e76
Evgeny <joy08@mail.ru>
2022-02-28 13:19:08 +1100

    Вторая лекция Язык програмирования

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 12, 31, 4, 18, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while(index < n)
+{
+
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; // останавлевает цикл
+    } 
+    //index = index + 1;
+    index++;
+}
